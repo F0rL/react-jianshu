92ed8fb2f746ce26adfd96c3120cd11867c2e962
Forl <kumaloveslife@gmail.com>
2019-08-31 14:35:23 +0800

    引入immutable指定state不可更改

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,15 +1,14 @@
-import * as constants from './constants'
+import * as constants from "./constants";
+import { fromJS } from "immutable";
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false
-}
+});
 
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    const isFocus = !state.focused
-    return {
-      focused: isFocus
-    }
+    const isFocus = !state.get('focused');
+    return state.set('focused', isFocus)
   }
-  return state
-}
+  return state;
+};
